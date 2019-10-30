1c5902673b4ff7bf8f243f9249f9c84a0679ba95
gersonEspinoza722 <39819821+gersonEspinoza722@users.noreply.github.com>
2019-10-29 18:31:41 -0600

    Attack Command done

--- src/Client/Game/GameNotification.java
+++ src/Client/Game/GameNotification.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 
 public class GameNotification extends Message implements Serializable {
 
-    public GameNotification(String sentBy, String event, Object objectOfInterest) {
+    public GameNotification(int sentBy, String event, Object objectOfInterest) {
         super(sentBy, event, objectOfInterest);
     }
 
